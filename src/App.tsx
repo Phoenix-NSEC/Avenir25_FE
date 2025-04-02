4d4b36d6190c2182511aee242ff8fe65046b42d0
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-04-02 15:38:39 +0530

    components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Events from "./components/Events";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Sponsers from "./components/Sponsors";
+import TimeLine from "./components/TimeLine";
+
 const App = () => {
-  return <div>Avenir FE</div>;
+  return <div>Avenir FE
+
+    {/* <Navbar/> */}
+    <Hero/>
+    <Events/>
+    <TimeLine/>
+    <About/>
+    <Sponsers/>
+    <ContactUs/>
+  </div>;
 };
 
 export default App;
