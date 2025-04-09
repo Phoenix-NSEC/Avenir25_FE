417e0ffe4fb4ca6d5db459ad2ca95668d66a8841
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-04-09 13:52:39 +0530

    update: team-data

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+// FAQ.tsx
+import React from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
-const FAQ = () => {
+import { faqData } from '../constants/faq-data';
+import { EnchantedHeading } from './EnchantedHeading';
+
+const FAQ: React.FC = () => {
   return (
-    <div>
-      FAQ
+    <div className="faq-container">
+      <h2 className="faq-title"><EnchantedHeading>Frequently Asked Questions</EnchantedHeading></h2>
+      <Accordion type="single" collapsible className='mx-32 -mx-10 my-32 '>
+        {faqData.map((faq, index) => (
+          <AccordionItem key={index} value={`item-${index}`}>
+            <AccordionTrigger className='text-xl'>{faq.question}</AccordionTrigger>
+            <AccordionContent className='text-lg'>{faq.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
