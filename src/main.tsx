6ecb8915155d5f4cc9bd53ba5257c5dfc01a26e9
debojitsaha <saha05debojit@gmail.com>
2025-05-01 16:39:40 +0530

    add: hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Team from './components/Team.tsx';
-import EventPage from './components/EventPage.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
+import App from "./App.tsx";
+import Team from "./components/Team.tsx";
+import EventPage from "./components/EventPage.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/team",
-    element: <Team/>,
-  },
-  {
-    path: "/events",
-    element: <EventPage/> 
-  }
-  
-]);
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <RouterProvider router={router} />
-    {/* <App /> */}
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/events" element={<EventPage />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
